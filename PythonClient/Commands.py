4a05d23914fa053e5fa570b9ff621491c61d10ed
Ninad Sheth <ninad.sheth@somaiya.edu>
2018-03-11 23:57:24 +0530

    Updated Rotate Command to use rate and improved algo to get better rotations
    Patched a workaround where drone would rotate from wrong side

--- PythonClient/Commands.py
+++ PythonClient/Commands.py
@@ -160,38 +160,66 @@ class CmdRotate(CmdBase):
         self.mystate_module = self.get_persistent_module('mystate')
         self.constants_module = self.get_persistent_module('constants')
         self.intent_provider_module = self.get_persistent_module('intent_provider')
-
+        self.full_rate = 60
+        self.low_rate = 10
         # Note here we get yaw in radians but later we set it in deg
         pitch, roll, yaw = AirSimClientBase.toEulerianAngle(self.mystate_module.get_orientation())
-        delta = float(self.line[2])*3.14/180
-        if self.line[1] == 'left':
-            yaw -= delta
-        elif self.line[1] == 'right':
-            yaw += delta
-        elif self.line[1] == 'to':
-            yaw = delta
+        if yaw < 0:
+            yaw = 2 * 3.14 + yaw
+        if (self.line[1] in ['left', 'right', 'to']):
+            delta = float(self.line[2])*3.14/180
+            if self.line[1] == 'left':
+                self.full_rate -= 1
+                self.low_rate -= 1
+                yaw -= delta
+            elif self.line[1] == 'right':
+                yaw += delta
+            elif self.line[1] == 'to':
+                diff = yaw - delta
+                side = 1 # right side
+                if diff >= 0 and diff < 3.14: # left side
+                    side = -1
+                self.full_rate *= side
+                self.low_rate *= side
+                yaw = delta
 
-        self.final_yaw = yaw
-
-        self.intent_provider_module.submit_intent(CmdMoveToPoint.__name__, PModHIntents.ROTATE, [pitch, roll, yaw])
-        # Note that this call is cancellable if other movement related call is called
-        self.get_client().rotateToYaw(self.final_yaw * 180 / 3.14, 0) # note that this fun uses in degrees (inconsistency)
+            self.final_yaw = yaw
+            self.intent_provider_module.submit_intent(CmdMoveToPoint.__name__, 
+                            PModHIntents.ROTATE, [pitch, roll, yaw])
+        else: # rate
+            self.rate = float(self.line[2])
+            self.intent_provider_module.submit_intent(CmdMoveToPoint.__name__, 
+                            PModHIntents.ROTATE, [self.rate])
 
     def update(self):
-        yaw = AirSimClientBase.toEulerianAngle(self.mystate_module.get_orientation())[2]
-        # Check if movement is complete or < 0.5 meters distance, anyway thats offset
-        #print(abs(self.final_yaw - yaw))
-        if abs(self.final_yaw - yaw) < 0.1:
-            self.intent_provider_module.mark_as_complete(CmdMoveToPoint.__name__)
-            if self.engage_object != None:
-                self.engage_object.mark_done()
-            return True
-        return False
+        if self.line[1] in ['left', 'right', 'to']:
+            yaw = AirSimClientBase.toEulerianAngle(self.mystate_module.get_orientation())[2]
+            if yaw < 0:
+                yaw = 2 * 3.14 + yaw
+            # Check if movement is complete or < 0.1 angle distance, anyway thats offset
+            # dist to angle
+            dist = min(abs(self.final_yaw - yaw), 2 * 3.14 - abs(self.final_yaw - yaw))
+            print('{0} {1} {2}'.format(self.final_yaw, yaw, dist))
+            if dist < 0.1:
+                self.get_client().hover()
+                self.intent_provider_module.mark_as_complete(CmdMoveToPoint.__name__)
+                if self.engage_object != None:
+                    self.engage_object.mark_done()
+                return True
+            # Note that this call is cancellable if other movement related call is called
+            if dist < 0.5:
+                self.get_client().rotateByYawRate(self.low_rate, 0.5) # note that this fun uses in degrees (inconsistency)
+            else: # on full rate
+                self.get_client().rotateByYawRate(self.full_rate, 0.5) # note that this fun uses in degrees (inconsistency)
+            return False
+        else: # Rate
+            self.get_client().rotateByYawRate(self.rate, 0.5)
+
 
     # Update other can_process
     def can_process(line):
         try:
-            if line[0] in ['turn'] and line[1] in ['left', 'right', 'to'] and type(float(line[2])) is float:
+            if line[0] in ['turn'] and line[1] in ['left', 'right', 'to', 'rate'] and type(float(line[2])) is float:
                 return True
             return False
         except: # some error only if command not proper
